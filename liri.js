85e4723108b96123d931ac2e8c88b5c47070e20c
Jack Morrissey <j.morrissey.16@gmail.com>
2018-10-15 21:30:40 -0500

    OMDB request functional

--- liri.js
+++ liri.js
@@ -22,13 +22,29 @@ function bandsRequest () {
             Venue Location: ${bandGuy[0].venue.city}, ${bandGuy[0].venue.region}, ${bandGuy[0].venue.country}
             Date of Event: ${time}`);
             console.log("=================================")
-        }
-    })
-} 
+        };
+    });
+}; 
 
 function omdbRequest () {
-    request()
-}
+    request("http://www.omdbapi.com/?apikey=trilogy&t=" + userSearch + "&plot=short", function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var movieGuy = JSON.parse(body);
+            console.log("================================");
+            console.log(`Results for ${userSearch}: 
+            Title: ${movieGuy.Title}
+            Year Released: ${movieGuy.Year}
+            IMDB Rating: ${movieGuy.imdbRating}
+            Rotten Tomatoes Rating: ${movieGuy.Ratings[1].Value}
+            Produced in: ${movieGuy.Country}
+            Language: ${movieGuy.Language}
+            Plot: ${movieGuy.Plot}
+            Actors: ${movieGuy.Actors}
+            `);
+            console.log("================================");
+        };
+    });
+};
 
 
 
@@ -38,7 +54,7 @@ if (command === "concert-this") {
 } else if (command === "spotify-this-song") {
     // spotifyFunction()
 } else if (command === "movie-this") {
-    // omdb function
+    omdbRequest();
 } else if (command === "do-what-it-says") {
     // fs pkg command
 } else {
